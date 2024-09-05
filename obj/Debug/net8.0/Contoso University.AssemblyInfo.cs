8f893eb89be7e4660cee79673e73ce970dd90f4d
juupant <amunt2002@gmail.com>
2024-09-05 21:54:13 +0300

    clone added
    added clone function, seems to work.

--- obj/Debug/net8.0/Contoso University.AssemblyInfo.cs
+++ obj/Debug/net8.0/Contoso University.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Contoso University")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9846300f277f9906f757f66e48341665c798717c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73171ca2982d2ce21ba6130b31ae50a2e436205f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Contoso University")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Contoso University")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
