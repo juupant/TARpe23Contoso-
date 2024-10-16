392aef9eb0b09aa0e1681727aa3cada8fa0517f4
juupant <amunt2002@gmail.com>
2024-10-16 20:59:16 +0300

    delete ja edit meetodid

--- Controllers/DepartmentsController.cs
+++ Controllers/DepartmentsController.cs
@@ -55,6 +55,67 @@ namespace Contoso_University.Controllers
             ViewData["InstructorID"] = new SelectList(_context.Instructors, "ID", "FullName", department.InstructorID);
             return View(department);
         }
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            string query = "SELECT * FROM Departments WHERE DepartmentID = {0}";
+            var department = await _context.Departments.FromSqlRaw(query, id).Include(d => d.Administrator).AsNoTracking().FirstOrDefaultAsync();
+            if (department == null)
+            {
+                return NotFound();
+            }
+            ViewData["InstructorID"] = new SelectList(_context.Instructors, "ID", "FullName");
+            return View(department);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit([Bind("DepartmentID,InstructorID,Name,Budget,StartDate,SuperImportantString")] Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                var existingDepartment = _context.Departments.AsNoTracking().FirstOrDefault(m => m.DepartmentID == department.DepartmentID);
+
+                if (existingDepartment == null)
+                {
+                    return NotFound();
+                }
+
+                _context.Departments.Update(department);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewData["InstructorID"] = new SelectList(_context.Instructors, "ID", "FullName");
+            return View(department);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            string query = "SELECT * FROM Departments WHERE DepartmentID = {0}";
+            var department = await _context.Departments.FromSqlRaw(query, id).Include(d => d.Administrator).AsNoTracking().FirstOrDefaultAsync();
+            if (department == null)
+            {
+                return NotFound();
+            }
+            return View(department);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int ID)
+        {
+            var department = await _context.Departments.FindAsync(ID);
+            _context.Departments.Remove(department);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
 
 
     }
